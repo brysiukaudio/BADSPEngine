eb451b026f63d2d2b3a9524bc6d55fc13b876df0
Ben Brysiuk <benbrysiuk@gmail.com>
2016-07-05 09:42:04 -0700

    Playback is working

--- BADSPEngine/Algorithms/FourtoTwoChannelMixer.cpp
+++ BADSPEngine/Algorithms/FourtoTwoChannelMixer.cpp
@@ -29,8 +29,10 @@ void FourtoTwoChannelMixer::process() {
 		this->m_poutBuffer[1][i] = (fGain1*this->m_pinBuffer[0][i]) + (fGain2*this->m_pinBuffer[1][i]) + (fGain3*this->m_pinBuffer[2][i]) + (fGain4*this->m_pinBuffer[3][i]);
 			}
 		}
-void FourtoTwoChannelMixer::setBuffers(float **inBuffer, float** outBuffer) {
 
+void FourtoTwoChannelMixer::setBuffers(float** inBuffer, float** outBuffer) {
+	this->setInputBuffer(inBuffer);
+	this->setOutputBuffer(outBuffer);
 }
 
 void FourtoTwoChannelMixer::setInputBuffer(float ** inBuffer) {
